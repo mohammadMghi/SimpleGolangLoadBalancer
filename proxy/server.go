20e06b0e427b8f8dbd44e04c8608ff5396272744
Mohammad <mohammadmghig@gmail.com>
2023-06-17 02:37:29 +0330

    load balancer

--- proxy/server.go
+++ proxy/server.go
@@ -1,9 +1,15 @@
 package proxy
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
+	"time"
+
+	"golang.org/x/tools/go/pointer"
 )
 
 func Server(){
@@ -16,11 +22,14 @@ func Server(){
 	revProxy := &httputil.ReverseProxy{
 		Director: d,
 	}
+	
+	go helthChecker()
 
 	ser := http.Server{
 		Addr: ":8080",
 		Handler : revProxy,
 	}
+
 
 
 	err := ser.ListenAndServe()
@@ -30,4 +39,34 @@ func Server(){
 	}
 	
 
+}
+
+func isUpService(url *url.URL) error{
+	connection , err := net.DialTimeout("tcp" , url.Host , time.Minute * 1) ; if err != nil {
+		return err
+	}
+	defer connection.Close()
+	return nil
+}
+
+func helthChecker (){
+	time := time.NewTicker(time.Minute * 1)
+	nodes :=  NewConfig().Nodes
+	for{
+		select{
+			case <-time.C:
+				for _ , node := range nodes {
+					nnode := &node
+					url, err := url.Parse(nnode.URL); if err != nil{
+						log.Fatal(err.Error())
+					} 
+					err = isUpService(url) ; if err != nil{
+						log.Fatal("node is down : %v :: %v" ,  err.Error() , nnode.URL )
+					}
+					fmt.Println("node is up ... %v"  , nnode.URL )
+
+
+				}
+		}
+	}
 }
